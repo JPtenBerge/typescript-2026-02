1c31b2a35fc3097e61c4441e147dabac38b82d3b
JP <jp@jp.nl>
2026-02-10 09:53:00 +0100

    Interfaces afgemaakt

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -35,11 +35,56 @@ let haarborstel: Product = {
 //     }
 // }
 
-interface Aaaa { getal: number; }
+interface Aaaa { getal: number; tekstje: string }
 interface Bbbb { getal: number; }
 
-let a: Aaaa = { getal: 14 };
+let a: Aaaa = { getal: 14, tekstje: 'qw' };
 let b: Bbbb = { getal: 28 };
 
-a = b; // structural typing
+a = b as Aaaa; // structural typing
+b = a;
 
+
+interface Dictionary {
+    [prop: string]: number;
+}
+
+let tellertjes: Dictionary = {};
+
+let tekstje = 'hallo ik ben jp en ik geef training vandaag';
+for (let character of tekstje) {
+    tellertjes[character] ??= 0;
+    tellertjes[character]++;
+}
+
+console.log(tellertjes['b']);
+// console.log(tellertjes.b);
+
+
+interface Bla {
+    get(query: string): string;
+    get(element: HTMLElement): number;
+}
+
+// jQuery
+// $('div');
+// $('<div>')
+// $(function() { }) // dom ready
+
+interface Cypress {
+    should(str: 'have.text'): boolean;
+    should(str: 'have.count'): boolean;
+    should(str: 'includes.text'): boolean;
+    // should(str: 'have.not.text'): boolean;
+    // should(str: 'have.text'): boolean;
+}
+
+let cy: Cypress = {
+    should(str: 'have.text' | 'have.count' | 'includes.text') {
+        
+        return true;
+    },
+    
+    
+};
+// cy.should()
